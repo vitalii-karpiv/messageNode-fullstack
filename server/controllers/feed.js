61ee525cdf41ff6e30c3cd86abb1ad8481e74741
newbie <newbie@newbies-MacBook-Pro.local>
2021-05-16 08:47:00 +0300

    create get all posts endpoint

--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -1,6 +1,15 @@
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: 'First Post', content: 'This is the first post!' }]
+    posts: [
+      {
+        _id: 1,
+        title: "First Post",
+        content: "This is the first post!",
+        imageUrl: "images/naruto.jpeg",
+        creator: { name: "Vitalii" },
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
@@ -9,7 +18,7 @@ exports.createPost = (req, res, next) => {
   const content = req.body.content;
   // Create post in db
   res.status(201).json({
-    message: 'Post created successfully!',
-    post: { id: new Date().toISOString(), title: title, content: content }
+    message: "Post created successfully!",
+    post: { id: new Date().toISOString(), title: title, content: content },
   });
 };
